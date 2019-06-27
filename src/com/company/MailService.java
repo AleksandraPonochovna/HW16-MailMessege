bbaf2544f675e7ad0d3c6cd02006a0854fe9238a
Aleksandra Ponochovna <aleksandra_ponochovna@outlook.com>
2019-06-27 13:23:14 +0300

    second commit

---
+++ src/com/company/MailService.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+public class MailService<T> implements Consumer<Sendable<T>> {
+
+    private Map<String, List<T>> mailBox;
+
+    private Map<String, List<T>> messagesMap = new HashMap<String, List<T>>(){
+        @Override
+        public List<T> get(Object key) {
+            if (this.containsKey(key)) {
+                return super.get(key);
+            } else {
+                return Collections.emptyList();
+            }
+        }
+    };
+
+    @Override
+    public void accept(Sendable<T> sendable) {
+        List<T> ts = messagesMap.get(sendable.getTo());
+        if (ts.size() == 0) {
+            ts = new ArrayList<>();
+        }
+        ts.add(sendable.getContent());
+        messagesMap.put(sendable.getTo(), ts);
+    }
+
+    public Map<String, List<T>> getMailBox() {
+        return messagesMap;
+    }
+}
---
+++ src/com/company/Salary.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Salary extends SendableClass<Integer> {
+    public Salary(String from, String to, Integer content) {
+        super(from, to, content);
+    }
+}
